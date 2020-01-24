24e9b673b7df4f503f4f3d631d6e4c5dd802b073
Ranjit R <1688982@TCS.com>
2020-01-24 17:26:52 +0530

    update some info

--- angular_project_creation.js
+++ angular_project_creation.js
@@ -6,3 +6,52 @@ angular project creation step
 Module generate with Routing
 ----------------------------
 ng g module authenticate-module --routing=true
+
+
+
+
+
+
+Github Comments:
+====================
+Basic Comments
+----------------
+
+git add foldername
+git commit -m "user message"
+
+git stash:
+----------
+=> git stash push -m "sample msg" - git stash for all changed files
+
+=> git stash push -m <message> <path-of-file1> <path-of-file2> - git stash for specific file 
+ 
+=> git stash list -  we can view list of stash commits
+
+=> git stash pop - undo git stash
+
+=> git stash pop stash@{n} (or) git stash pop n - undo the specific stash commits
+
+=> git stash show – will show the list of files changed in ‘last’ stash.
+
+=> git stash show n – will show the list of files changed in ‘n th’ stash.
+
+=> git stash show -p – will preview the git diff contents of the ‘last’ stash.
+
+=> git stash show -p n -will preview the git diff contents in the ‘n th’ stash.
+
+
+
+Hi Murali,
+
+Iam working in IHG project since (05/06/2019) request you to release me from the project.
+
+I’m glad to have been a part of IHG in which I got a chance to work in the web support team, but now I’d like to work in another project 
+which has a lot of scope exclusively for development in (Angular 6+, React js) Business Intelligence & (Node js projects) the upcoming new technology,
+
+As my interest lies in FullStack development project, I’d like to work in the development project where there is more scope for Angular 6+ with Node Js development 
+project. This would not only give me self-satisfaction but also help me to build a very strong career in TCS.
+
+Also, I travel almost for 2 hrs to from my home in (HOME AREA) to office in (OFFICE AREA),so I was thinking of getting into a project which is operated from facilities near to my home, which would help me in reducing the time I invest in traveling.
+
+I kindly request you to consider my request and do the needful at the earliest.
